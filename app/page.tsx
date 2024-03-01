033e66818f31f4b0754c450922a23f4100fc5164
scotty974 <fabienethevepro@gmail.com>
2024-03-01 20:51:16 +0100

    div follow mouse

--- app/page.tsx
+++ app/page.tsx
@@ -4,35 +4,44 @@ import { Canvas } from "@react-three/fiber";
 import Sphere from "./components/Cube/Cube";
 import TextDrei from "./components/TextDrei/TextDrei";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 export default function Home() {
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
+  const [cursorVariant, setcursorVariant] = useState("default");
 
   useEffect(() => {
-    const updateMousePosition = (event: React.MouseEvent) => {
+    const updateMousePosition = (event: MouseEvent) => {
       setMouse({
         x: event.clientX,
         y: event.clientY,
       });
     };
-    window.addEventListener("mousemove", updateMousePosition);
 
+    window.addEventListener("mousemove", updateMousePosition);
     return () => {
       window.removeEventListener("mousemove", updateMousePosition);
+      
     };
-  });
+  }, []);
 
+  const variants: any = {
+    default: {
+      x: mouse.x - 16,
+      y: mouse.y - 16,
+    },
+  };
   return (
     <main>
       <section className="container m-auto">
         <Header></Header>
       </section>
-      <section>
+      <motion.section>
         <div className="flex justify-center items-center">
           <Canvas
             shadows
             camera={{ position: [0, 0, 20], fov: 50 }}
             eventPrefix="client"
-            className="absolute"
+            className="absolute canvas-container "
           >
             <ambientLight intensity={0.1} />
             <spotLight
@@ -45,7 +54,12 @@ export default function Home() {
             <Sphere></Sphere>
           </Canvas>
         </div>
-      </section>
+        <motion.div
+          className="cursor"
+          variants={variants}
+          animate={cursorVariant}
+        ></motion.div>
+      </motion.section>
     </main>
   );
 }
